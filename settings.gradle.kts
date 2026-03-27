b616f3872ac9a2b409e5820fd6f06885d40bd339
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-03-27 18:14:43 -0400

    Create project with simple defaults

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("androidx.*")
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+            }
+        }
+
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+rootProject.name = "SilentSync"
+include(":app")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
